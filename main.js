208afeb7a48754fb6c72a6550f35cd9e4aeb1994
insu <ohinsu134@gmail.com>
2021-03-11 22:53:59 +0900

    almost done keyCode

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ class Degit {
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.strokeStyle = this.color;
+    ctx.strokeStyle = COLOR2;
     for(let i = 0; i < 3; i++) {
       for(let i2 = 0; i2 < 3; i2++) {
         ctx.arc(this.x + (i * (this.maxWidth/45)), this.y + (i2 * (this.maxHeight / 15)), this.size, 0, Math.PI * 2);
@@ -37,7 +37,7 @@ class Degit {
     setTimeout(() => {
       this.color = COLOR2;
       this.draw();
-    }, 1000);
+    }, 500);
   }
 
 }
@@ -66,13 +66,74 @@ function declareKeyCode() {
   degits[11].keyCode = 189;
   degits[12].keyCode = 187;
   degits[13].keyCode = 8;
+
+  degits[14].keyCode = 9;
+  degits[15].keyCode = 81;
+  degits[16].keyCode = 87;
+  degits[17].keyCode = 69;
+  degits[18].keyCode = 82;
+  degits[19].keyCode = 84;
+  degits[20].keyCode = 89;
+  degits[21].keyCode = 85;
+  degits[22].keyCode = 73;
+  degits[23].keyCode = 79;
+  degits[24].keyCode = 80;
+  degits[25].keyCode = 219;
+  degits[26].keyCode = 221;
+  degits[27].keyCode = 220;
+    
+  degits[28].keyCode = 17;
+  degits[29].keyCode = 65;
+  degits[30].keyCode = 83;
+  degits[31].keyCode = 68;
+  degits[32].keyCode = 70;
+  degits[33].keyCode = 71;
+  degits[34].keyCode = 72;
+  degits[35].keyCode = 74;
+  degits[36].keyCode = 75;
+  degits[37].keyCode = 76;
+  degits[38].keyCode = 186;
+  degits[39].keyCode = 222;
+  degits[40].keyCode = 13;
+  degits[41].keyCode = 13;
+  
+  degits[42].keyCode = 16;
+  degits[43].keyCode = 16;
+  degits[44].keyCode = 90;
+  degits[45].keyCode = 88;
+  degits[46].keyCode = 67;
+  degits[47].keyCode = 86;
+  degits[48].keyCode = 66;
+  degits[49].keyCode = 78;
+  degits[50].keyCode = 77;
+  degits[51].keyCode = 188;
+  degits[52].keyCode = 190;
+  degits[53].keyCode = 191;
+  degits[54].keyCode = 16;
+  
+  degits[56].keyCode = 20;
+  degits[57].keyCode = 18;
+  degits[58].keyCode = 91;
+  degits[59].keyCode = 32;
+  degits[60].keyCode = 32;
+  degits[61].keyCode = 32;
+  degits[62].keyCode = 32;
+  degits[63].keyCode = 32;
+  degits[64].keyCode = 32;
+  degits[65].keyCode = 32;
+  degits[66].keyCode = 38;
+
+  degits[67].keyCode = 93;
+  degits[68].keyCode = 18;
+  degits[69].keyCode = 37;
+
+
 }
 
 function keyIndi(event) {
+  console.log(event.keyCode);
   degits.forEach((value, index) => {
-    if(index != 0 && -1) {
     value.keyCode == event.keyCode ? value.wink() : false;
-    }
   });
 }
 
